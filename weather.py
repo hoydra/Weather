d9729253cda16664ffae4a3510078db6a4474c47
hoydra <77149251+hoydra@users.noreply.github.com>
2021-07-11 21:17:04 +0200

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,28 @@
+import requests
+
+city = input("City/Country: ")
+
+api_key = "your api"
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+res = requests.get(url)
+
+jsonfile = res.json()
+try:
+    kelvin = jsonfile["main"]["temp"]
+    cond = jsonfile["weather"][0]["main"]
+    place = jsonfile["name"]
+
+    celsius = kelvin -  273.15
+    celsius = round(celsius,2)
+
+
+    currently = {"place":place,"temperature":celsius,"condition":cond}
+
+    print(currently)
+
+except Exception as e:
+
+    print("An Error Has Occurred:\nPlease double-check that your 'City/Country:' exists\nTroubleshoot to rcellini8@gmail.com if something is wrong")
+
+
+
